2c136e829381b0b371b6bd60744af6052347e587
francescofalcone05 <ciccio_97@live.it>
2024-05-02 12:08:10 +0300

    difficolty-added

--- js/main.js
+++ js/main.js
@@ -4,26 +4,72 @@
 let start = document.getElementById('btn');
 //creo una variabile con il contenitore della griglia
 let tabella = document.getElementById('campo');
+//creo variabile per il select
+let level = document.getElementById('difficolty');
 
 start.addEventListener('click', function () {
 
-    addAndRemove(tabella, start)
-    //rimuovo la classe hide dalla tabella
-    //tabella.classList.remove('hide')
+    /*se voglio tenere bottone e select in pagina durante il gioco,
+     per poter cambiare il livello anche dopo averlo selezionato la prima volta */ 
+    tabella.innerHTML = ""
 
-    //aggiungo la classe hide al bottone
-    // start.classList.add('hide')
+    //se il livello selezionato e` Easy
+    if (level.value == 'easy') {
+        // rimuovo la classe hide dalla tabella
+        tabella.classList.remove('hide')
+        //aggiungi la classe container al contenitore a secondo del livello selezionato
+        tabella.classList.add('contenitore_easy')
+
+        //se voglio nascondere bottone e select una volta scelto il livello aggiungo la classe hide
+        //start.classList.add('hide')
+        //level.classList.add('hide')
+
+        for (let i = 1; i < 101; i++) {
+
+            //richiamo funzione
+            let elemento = creaElemento(i);
+
+        }
+
+    //altrimenti se il livello selezionato e` Medium
+    } else if (level.value == 'medium') {
+        // rimuovo la classe hide dalla tabella
+        tabella.classList.remove('hide')
+        //aggiungi la classe container al contenitore a secondo del livello selezionato
+        tabella.classList.add('contenitore_medium')
+
+        //se voglio nascondere bottone e select una volta scelto il livello aggiungo la classe hide
+        //start.classList.add('hide')
+        //level.classList.add('hide')
+
+        for (let i = 1; i < 82; i++) {
+
+            //richiamo funzione
+            let elemento = creaElemento(i);
+
+        }
+    //altrimenti se il livello selezionato e` Hard
+    } else if (level.value == 'hard') {
+        // rimuovo la classe hide dalla tabella
+        tabella.classList.remove('hide')
+        //aggiungi la classe container al contenitore a secondo del livello selezionato
+        tabella.classList.add('contenitore_hard')
+
+        //se voglio nascondere bottone e select una volta scelto il livello aggiungo la classe hide
+        //start.classList.add('hide')
+        //level.classList.add('hide')
+
+        for (let i = 1; i < 50; i++) {
+
+            //richiamo funzione
+            let elemento = creaElemento(i);
+
+        }
+
+    }
 
 })
 
-
-
-for (let i = 1; i < 101; i++) {
-
-    //richiamo funzione
-    let elemento = creaElemento(i);
-
-}
 
 /**************FUNCTIONS****************/
 
@@ -47,16 +93,16 @@ function creaElemento(numero) {
 
         //aggiungi o rimuovi la classe color
         div.classList.toggle('color');
-        console.log(numero)
+        console.log('ho cliccato', numero)
 
         //se dentro l'innerText non c'e` nulla
         if (div.innerText == '') {
             //aggiungi inerText
             div.innerText = numero;
         } else {
-            
+
             //altrimenti svuota innerText
-            div.innerText = '' 
+            div.innerText = ''
         }
     });
 
@@ -64,17 +110,7 @@ function creaElemento(numero) {
 
 };
 
-function addAndRemove(elemento1, elemento2) {
 
-    //rimuovo la classe hide 
-    elemento1.classList.remove('hide');
-
-    //aggiungo la classe hide 
-    elemento2.classList.add('hide');
-
-    return elemento1, elemento2
-
-}
 
 
 /***************************************/
--- readme.md
+++ readme.md
@@ -15,3 +15,20 @@ Aggiungete la gestione dell'evento di click, che stampi il numero e cambi il col
 
 MILESTONE #4
 Spostate alcuni elementi in funzioni, ad esempio la creazione del quadrato e/o l'event listener.
+
+BONUS #1: CAPIRE LO SCOPE
+In milestone #3 stampate il numero della cella al click, prendendolo dal contenuto della cella. Qualcosa del tipo cella.innerHtml.
+Provate adesso a modificare il ciclo che crea le vostre celle facendo in modo che le celle siano vuote, non contengano nessun numero.
+Come fate adesso a stampare qualcosa al click? Nello scope dell'event-handler ho qualche variabile visibile che mi può indicare il numero?
+E se provo a controllare lo scope "padre" dell'event handler?
+
+BONUS #2: CAPIRE EVENTI E FUNZIONI
+Provate a eseguire quanto fatto fin qui NON al caricamento della pagina, ma al click su un pulsante "Avvia gioco".
+
+BONUS #3: QUANDO IL GIOCO SI FA DURO
+Aggiungere una select accanto al bottone di start. La select presenterà tre difficoltà di gioco tra cui scegliere (easy, medium, hard).
+Quando generate la griglia userete un numero di celle che dipende dalla difficoltà.
+Easy: 100 celle (10x10)
+Medium: 81 celle (9x9)
+Hard: 49 (7x7)
+Ragionate bene su quando vi serve conoscere la selezione dell'utente: è in quel momento che dovrete recuperare il dato.
